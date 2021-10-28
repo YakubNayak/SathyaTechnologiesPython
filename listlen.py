f72c42997ac05f03e461e10c1320e821c7b97883
YakubNayak <90748772+YakubNayak@users.noreply.github.com>
2021-10-28 19:35:36 +0530

    Delete listlen.py

--- listlen.py
+++
@@ -1,9 +0,0 @@
-animals=['Tiger','Lion','Dog','Cat','rat']
-print("Bofore sort Animals Are=",animals)
-animals.sort(reverse=True)
-print("After sort Animals Are=",animals)
-no=[3,0,23,89,0,23,98,56]
-no.sort(reverse=True)
-print(no)
-
-
