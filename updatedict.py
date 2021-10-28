5da60b434ff6c0c5ff0b8887345189fc494c1a27
YakubNayak <90748772+YakubNayak@users.noreply.github.com>
2021-10-28 19:31:15 +0530

    Delete updatedict.py

--- updatedict.py
+++
@@ -1,6 +0,0 @@
-s={'id':"[1,2,3,4]",'name':"('ravi','raju')",'state':"{'Tealangana','panjab','rajasthan'}"}
-n = {'id' : [1,2,3], 'name' : ('Krishna','Rama'), 'Age' : {22,21}}
-
-print(s)
-print(n)
-
